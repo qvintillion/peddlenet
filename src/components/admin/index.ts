a751406db13b63d72b652f533afb8af8d8e114d0
qvintillion <wjcostellov@gmail.com>
2025-06-16 23:27:36 -0500

    Add admin components index.ts for proper imports

---
+++ src/components/admin/index.ts
@@ -0,0 +1,8 @@
+// Admin component exports
+export { ActivityFeed } from './ActivityFeed';
+export { AdminControls } from './AdminControls';
+export { DetailedUserView } from './DetailedUserView';
+export { DetailedRoomView } from './DetailedRoomView';
+export { MeshNetworkStatus } from './MeshNetworkStatus';
+export { MeshNetworkTest } from './MeshNetworkTest';
+export { PageWithMeshTesting } from './PageWithMeshTesting';
